2f5785955c83d7e538870dad1b0f522a83b73f26
skeletronys <mykytcey@gmail.com>
2024-07-30 19:01:19 +0200

    Added standart views & urls

--- Libary/views.py
+++ Libary/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from Libary.models import Book, CustomUser, Borrowing, Payment
+from Libary.serializers import (
+    BookSerializer,
+    CustomUserSerializer,
+    BorrowingSerializer,
+    PaymentSerializer,
+)
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
