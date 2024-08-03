ec46ff1f80f7d4192b47754bed1c0e30a029d86e
skeletronys <mykytcey@gmail.com>
2024-08-03 03:08:14 +0200

    Added validators for Borrow, Expected, Actual, dates

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+from borrowings.validators import date_limit_take_book, date_limit_return_book
 from user.models import User
 
 
 class Borrowing(models.Model):
-    Borrow = models.DateField()
-    Expected_return_date = models.DateField()
-    Actual_return_date = models.DateField(null=True, blank=True)
+    Borrow = models.DateField(validators=[date_limit_take_book])
+    Expected_return_date = models.DateField(validators=[date_limit_take_book])
+    Actual_return_date = models.DateField(
+        null=True, blank=True, validators=[date_limit_return_book]
+    )
     Book = models.ForeignKey(
         "Libary.Book", on_delete=models.CASCADE, related_name="Borrowing"
     )
---
+++ borrowings/validators.py
@@ -0,0 +1,17 @@
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from datetime import timedelta
+
+
+def date_limit_take_book(value):
+    limit = 30
+    if value > timezone.now().date() + timedelta(days=limit):
+        raise ValidationError(f"The date cannot be more than {limit} days from today.")
+
+
+def date_limit_return_book(value):
+    limit = 60
+    if value > timezone.now().date() + timedelta(days=limit):
+        raise ValidationError(
+            f"It is not possible to return the book after this {limit} days without a large fine"
+        )
