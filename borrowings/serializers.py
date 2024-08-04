efb8f86327d5be4fbcc19a7bd18e4decdaec4206
skeletronys <mykytcey@gmail.com>
2024-08-04 03:01:46 +0200

    Added validate if expected_date or actual_date < borrow raise error

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,22 +1,40 @@
 from rest_framework import serializers
-
 from Libary.models import Book
-
 from borrowings.models import Borrowing
-
-from user.models import User
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    Book = serializers.SlugRelatedField(queryset=Book.objects.all(), slug_field="name")
-    User = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    book = serializers.SlugRelatedField(queryset=Book.objects.all(), slug_field="name")
+    user = serializers.SerializerMethodField()
 
     class Meta:
         model = Borrowing
         fields = "__all__"
+        read_only_fields = ["user"]
+
+    def get_user(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}"
+
+    def validate(self, data):
+        if data["expected_return_date"] < data["borrow"]:
+            raise serializers.ValidationError(
+                "Expected return date cannot be before borrow date."
+            )
+        if "actual_return_date" in data and data["actual_return_date"] < data["borrow"]:
+            raise serializers.ValidationError(
+                "Actual return date cannot be before borrow date."
+            )
+        return data
 
     def create(self, validated_data):
-        book = validated_data.get("Book")
+        request = self.context.get("request")
+        if not request or not request.user:
+            raise serializers.ValidationError("User is not authenticated.")
+
+        book = validated_data.get("book")
+        if not book:
+            raise serializers.ValidationError("Book is required.")
+
         if book.inventory >= 1:
             book.inventory -= 1
             book.save()
@@ -24,10 +42,10 @@ class BorrowingSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("This book is currently out of stock.")
 
         borrowing = Borrowing.objects.create(
-            Book=book,
-            User=self.context["request"].user,
-            Borrow=validated_data["Borrow"],
-            Expected_return_date=validated_data["Expected_return_date"],
+            book=book,
+            user=request.user,
+            borrow=validated_data["borrow"],
+            expected_return_date=validated_data["expected_return_date"],
         )
 
         return borrowing
