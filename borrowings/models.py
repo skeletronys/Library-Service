5c3e7e601747cdb52ecef546694dd701ace8da6b
skeletronys <mykytcey@gmail.com>
2024-08-04 02:45:44 +0200

    Changed names variables in lower

--- borrowings/models.py
+++ borrowings/models.py
@@ -5,15 +5,15 @@ from user.models import User
 
 
 class Borrowing(models.Model):
-    Borrow = models.DateField(validators=[date_limit_take_book])
-    Expected_return_date = models.DateField(validators=[date_limit_take_book])
-    Actual_return_date = models.DateField(
+    borrow = models.DateField(validators=[date_limit_take_book])
+    expected_return_date = models.DateField(validators=[date_limit_take_book])
+    actual_return_date = models.DateField(
         null=True, blank=True, validators=[date_limit_return_book]
     )
-    Book = models.ForeignKey(
+    book = models.ForeignKey(
         "Libary.Book", on_delete=models.CASCADE, related_name="Borrowing"
     )
-    User = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Borrowing")
+    bser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Borrowing")
 
     def __str__(self):
         return f"{self.User.first_name} {self.User.last_name}"
