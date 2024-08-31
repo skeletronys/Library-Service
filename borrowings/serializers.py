ef1f3ea9ad31737b8199493ecd50cceec9d37279
skeletronys <mykytcey@gmail.com>
2024-08-31 02:09:46 +0200

    Added new logic for payment, added for Fine overdue payment.

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,7 +1,11 @@
 import datetime
+import stripe
 
+from decimal import Decimal
+from django.urls.base import reverse
 from rest_framework import serializers
 from Libary.models import Book
+from Libary_Service import settings
 from borrowings.models import Borrowing, Payment
 
 
@@ -14,12 +18,71 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
+        read_only_fields = [
+            "status",
+            "type",
+            "session_url",
+            "session_id",
+            "money_to_pay",
+        ]
+
+    def create(self, validated_data):
+        borrowing = validated_data.get("borrowing")
+        request = self.context.get("request")
+        success_url = (
+            request.build_absolute_uri(reverse("borrowings:payment-success"))
+            + "?session_id={CHECKOUT_SESSION_ID}"
+        )
+        cancel_url = request.build_absolute_uri(reverse("borrowings:payment-cancel"))
+
+        stripe.api_key = settings.STRIPE_API_KEY
+        days_borrowed = (borrowing.expected_return_date - borrowing.borrow).days
+
+        if borrowing.expected_return_date < datetime.date.today():
+            overdue_days = (datetime.date.today() - borrowing.expected_return_date).days
+            fine_amount = overdue_days * Decimal(borrowing.book.daily_fee) * Decimal(
+                settings.FINE_MULTIPLIER
+            ) + Decimal(days_borrowed) * Decimal(borrowing.book.daily_fee)
+            total_price = fine_amount
+            payment_type = Payment.Types.FINE
+        else:
+            total_price = Decimal(days_borrowed) * Decimal(borrowing.book.daily_fee)
+            payment_type = Payment.Types.PAYMENT
+
+        session = stripe.checkout.Session.create(
+            payment_method_types=["card"],
+            line_items=[
+                {
+                    "price_data": {
+                        "currency": "usd",
+                        "product_data": {
+                            "name": borrowing.book.name,
+                        },
+                        "unit_amount": int(total_price * 100),
+                    },
+                    "quantity": 1,
+                }
+            ],
+            mode="payment",
+            success_url=success_url,
+            cancel_url=cancel_url,
+        )
+
+        payment = Payment.objects.create(
+            borrowing=borrowing,
+            status=Payment.StatusType.PENDING,
+            type=payment_type,
+            session_url=session.url,
+            session_id=session.id,
+            money_to_pay=int(total_price),
+        )
+
+        return payment
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
     book = serializers.SlugRelatedField(queryset=Book.objects.all(), slug_field="name")
     user = serializers.SerializerMethodField()
-    payments = PaymentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Borrowing
@@ -72,6 +135,21 @@ class BorrowingSerializer(serializers.ModelSerializer):
             )
 
         instance.actual_return_date = datetime.date.today()
+
+        if instance.actual_return_date > instance.expected_return_date:
+            overdue_days = (
+                instance.actual_return_date - instance.expected_return_date
+            ).days
+            fine_amount = (
+                overdue_days * instance.book.daily_fee * settings.FINE_MULTIPLIER
+            )
+
+            Payment.objects.create(
+                borrowing=instance,
+                status=Payment.StatusType.PENDING,
+                type=Payment.Types.FINE,
+                money_to_pay=fine_amount,
+            )
 
         instance.book.inventory += 1
         instance.book.save()
